99c5042f764b4e3e084a52d0c86d71894b8ea087
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2023-07-17 22:20:22 +0200

    done

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addContact } from 'redux/operations';
+import { selectContacts } from 'redux/selectors';
+import { Form, Input, Text, Button } from './ContactForm.styled';
+
+const ContactForm = () => {
+  const dispatch = useDispatch();
+  const contacts = useSelector(selectContacts);
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    const contact = {
+      name: name,
+      number: number,
+    };
+
+    const isContactExist = contacts.find(
+      ({ name }) => name.toLowerCase() === contact.name.toLowerCase()
+    );
+
+    if (isContactExist) {
+      alert(`Contact with name ${contact.name} already exists!`);
+      return;
+    }
+
+    const isNumberExist = contacts.find(
+      ({ number }) =>
+        contact.number.replace(/\D/g, '') === number.replace(/\D/g, '')
+    );
+
+    if (isNumberExist) {
+      alert(`Number ${contact.number} is already in contacts!`);
+      return;
+    }
+
+    dispatch(addContact(contact));
+    setName('');
+    setNumber('');
+  };
+
+  const handleNameChange = event => {
+    setName(event.target.value);
+  };
+
+  const handleNumberChange = event => {
+    setNumber(event.target.value);
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Text>Name</Text>
+      <Input
+        type="text"
+        name="name"
+        placeholder="Enter name"
+        value={name}
+        onChange={handleNameChange}
+        pattern="^[a-zA-Zа-яА-Я]+([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+      />
+
+      <Text>Number</Text>
+      <Input
+        type="tel"
+        name="number"
+        placeholder="Enter phone number"
+        value={number}
+        onChange={handleNumberChange}
+        phonePattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+      />
+
+      <Button type="submit">
+        Add
+      </Button>
+    </Form>
+  );
+};
+
+export default ContactForm;
---
+++ src/components/ContactItem/ContactItem.jsx
@@ -0,0 +1,32 @@
+import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
+import { deleteContact } from 'redux/operations';
+import {
+  ContactItems,
+  ContactName,
+  ContactNumber,
+  Button,
+} from './ContactItem.styled';
+
+function ContactItem({ contact }) {
+  const dispatch = useDispatch();
+
+  const handleDelete = () => {
+    dispatch(deleteContact(contact.id));
+  };
+
+  return (
+    <ContactItems>
+      <ContactName>{contact.name}</ContactName>
+      <ContactNumber>{contact.number}</ContactNumber>
+      <Button onClick={handleDelete}>
+        Remove
+      </Button>
+    </ContactItems>
+  );
+}
+ContactItem.propTypes = {
+  contact: PropTypes.object.isRequired,
+};
+
+export default ContactItem;
